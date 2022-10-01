2753e52810ea90875e2ac8d7db7d9c874e19d215
Veri <vikas.yadav23@student.amity.edu>
2022-10-02 01:07:54 +0530

    Revert "Initial commit"
    This reverts commit a9b6240f0d9161f1f0ac24c3c43af5bcc4fc70fd.

--- src/main/java/com/learning/springweb/entities/repos/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learning.springweb.entities.repos;
-
-import com.learning.springweb.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
