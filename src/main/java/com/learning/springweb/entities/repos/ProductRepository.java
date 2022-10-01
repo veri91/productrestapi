c27d25f42d96d81a0394acfe184b60143d42a07e
Veri <vikas.yadav23@student.amity.edu>
2022-10-02 01:19:44 +0530

    Test Update Product

---
+++ src/main/java/com/learning/springweb/entities/repos/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.learning.springweb.entities.repos;
+
+import com.learning.springweb.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
